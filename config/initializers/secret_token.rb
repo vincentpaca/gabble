dc251ef0803659ef2dcad4dd8555d0025ab35ca0
Vincent Paca <vincent.paca@gmail.com>
2013-03-26 05:40:46 +0800

    Added initial Rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gabble::Application.config.secret_token = 'f49cc1edd59cc2e1c0dc7a9c167e2ee7436f2ea9b7dd24b73e08a6951d06516d291b8fff1265c3dd0833dc863360b88fffeb169d2ce36f503814d93516941dae'
