4ee5c76719f8b1f39bfce99eec785eeff9d4a980
Vincent Paca <vincent.paca@gmail.com>
2013-03-27 04:28:24 +0800

    Use different secret token for other environments aside production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,10 @@
-secret = ENV['GABBLE_SECRET']
-if secret.length < 30
-  raise "Secret token cannot be loaded"
+if Rails.env.production?
+  secret = ENV['GABBLE_SECRET']
+  if secret.length < 30
+    raise "Secret token cannot be loaded"
+  else
+    Gabble::Application.config.secret_token =  secret
+  end
 else
-  Gabble::Application.config.secret_token =  secret
+  Gabble::Application.config.secret_token =  "2inobn0zskftfqw991izhsy1wtxa25kzui46vihh6cjxbdeaok1qm7h2iiluyetyvquhn8bqx4l"
 end
